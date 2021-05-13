4ef7374025530cd855984bd6516ed4a86ca6994f
ashugowda <54936438+ashugowda@users.noreply.github.com>
2021-05-13 11:17:16 +0530

    Update WOABAT.py

--- optimizers/WOABAT.py
+++ optimizers/WOABAT.py
@@ -147,10 +147,7 @@ def WOABAT(objf,lb,ub,dim,SearchAgents_no,Max_iter,k,points, metric):
                        
                         if random.random() > r:
                             z[i,:] = Leader_pos[j] + 0.001 * numpy.random.randn(dim)
-                            if objf.__name__ == 'TWCV':
-                                fitnessValue,labelsPredValues= objf(startpts, points, k,metric) 
-                            else:
-                                fitnessValue,labelsPredValues= objf(startpts, points, k, metric) 
+                            fitnessValue,labelsPredValues= objf(startpts, points, k, metric) 
                             # Evaluate new solutions
                             #Fnew = objf(z[i, :])
                             '''
